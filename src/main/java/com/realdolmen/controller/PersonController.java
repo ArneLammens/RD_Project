1a6474bccafd8bd8d9f0fc04cee59be1bbd1121b
Arne <arne.lammens@gmail.com>
2014-10-01 13:35:01 +0200

    refactor user to person

--- src/main/java/com/realdolmen/controller/PersonController.java
+++ src/main/java/com/realdolmen/controller/PersonController.java
@@ -3,5 +3,5 @@ package com.realdolmen.controller;
 /**
  * Created by ALMAU78 on 1/10/2014.
  */
-public class UserController {
+public class PersonController {
 }
--- src/main/java/com/realdolmen/domain/user/PersonService.java
+++ src/main/java/com/realdolmen/domain/user/PersonService.java
@@ -3,5 +3,5 @@ package com.realdolmen.domain.user;
 /**
  * Created by ALMAU78 on 1/10/2014.
  */
-public class UserRepository {
+public class PersonService {
 }
