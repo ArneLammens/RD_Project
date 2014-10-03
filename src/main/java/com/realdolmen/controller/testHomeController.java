86297564863dcf59469fe36d165253d17f2ed3e2
Arne <arne.lammens@gmail.com>
2014-10-03 10:38:50 +0200

    arquillian

---
+++ src/main/java/com/realdolmen/controller/testHomeController.java
@@ -0,0 +1,47 @@
+package com.realdolmen.controller;
+
+import com.realdolmen.domain.Enums;
+import com.realdolmen.util.LoggerResources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class testHomeController implements Serializable {
+
+
+    private String tester;
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    public void init()
+    {
+        tester = "dit is een ingevulde waarde";
+
+    }
+
+    public void loggertest()
+    {
+        logger.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    }
+
+    public Enums.Region[] getRegions() {
+        return Enums.Region.values();
+    }
+
+    public String getTester() {
+        return tester;
+    }
+
+    public void setTester(String tester) {
+        this.tester = tester;
+    }
+}
---
+++ src/test/java/login/LoginSessionTest.java
@@ -0,0 +1,42 @@
+package login;
+
+import com.realdolmen.domain.Enums;
+import com.realdolmen.domain.location.LocationRepository;
+import com.realdolmen.domain.login.Login;
+import com.realdolmen.session.LoginSession;
+import common.AbstractArquillianTestCase;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class LoginSessionTest extends AbstractArquillianTestCase {
+
+    private LoginSession loginSession = new LoginSession();
+
+    @Deployment
+    public static JavaArchive createTestArchive() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage("com.realdolmen.session")
+                .addPackage("com.realdolmen.domain")
+                .addAsResource("META-INF/testPersistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void testAsync() throws Exception {
+        Login insertedLogin = new Login("admin", "admin", Enums.Roles.ADMIN);
+        loginSession.setLogin(insertedLogin);
+        Login retrievedLoginFromSession = loginSession.getLogin();
+        Assert.assertEquals(insertedLogin, retrievedLoginFromSession);
+    }
+
+
+}
